bbf0a5582acf3cd902540382cf038b92a1fc73c8
Ahmad Wasis <ahmadwasissh@gmail.com>
2026-03-10 15:08:03 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,22 +3,38 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class PaymentRepository {
+    private List<Payment> paymentData = new ArrayList<>();
+
     public Payment save(Payment payment) {
-        // TODO: implement
-        return null;
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
-        // TODO: implement
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
 
     public List<Payment> findAll() {
-        // TODO: implement
-        return null;
+        return new ArrayList<>(paymentData);
     }
 }
