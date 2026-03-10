7c07a81a1c946fc186fd5925309372c0e858ac80
Ahmad Wasis <ahmadwasissh@gmail.com>
2026-03-10 15:07:41 +0700

    [RED] Add PaymentRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PaymentRepository {
+    public Payment save(Payment payment) {
+        // TODO: implement
+        return null;
+    }
+
+    public Payment findById(String id) {
+        // TODO: implement
+        return null;
+    }
+
+    public List<Payment> findAll() {
+        // TODO: implement
+        return null;
+    }
+}
